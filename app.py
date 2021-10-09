919f10b171339e3fd1a1de7ae06d109d5aef6d41
John0520dd <2640890508@qq.com>
2021-10-09 08:11:38 +0800

    what the hell

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,url_for,redirect,flash
 from flask import url_for
 from flask_sqlalchemy import SQLAlchemy
 import config
@@ -30,11 +30,60 @@ def inject_user(): # 函数名可以随意修改
     return dict(user=user) # 需要返回字典，等同于return {'user': user}
 
 @app.route('/')
-def index():
+def hello():
     # user = User.query.first() # 读取用户记录
     movies = Movie.query.all() # 读取所有电影记录
     # return render_template('index1.html', movies=movies)
     return render_template('index.html', movies=movies)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST': # 判断是否是 POST 请求 # 获取表单数据
+        title = request.form.get('title') # 传入表单对应输入字段的 name 值
+        year = request.form.get('year') # 验证数据
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input.') # 显示错误提示
+            return redirect(url_for('index')) # 重定向回主页
+
+        # 保存表单数据到数据库
+        movie = Movie(title=title, year=year) # 创建记录
+        db.session.add(movie) # 添加到数据库会话
+        db.session.commit() # 提交数据库会话
+        flash('Item created.') # 显示成功创建的提示
+        return redirect(url_for('hello')) # 重定向回主页
+        user = User.query.first()
+        movies = Movie.query.all()
+    return render_template('index.html', user=user, movies=movies)
+
+
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+
+    if request.method == 'POST': # 处理编辑表单的提交请求
+        title = request.form['title']
+        year = request.form['year']
+
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input.')
+            return redirect(url_for('edit', movie_id=movie_id)) # 重定向回对应的编辑页面
+
+        movie.title = title # 更新标题
+        movie.year = year # 更新年份
+        db.session.commit() # 提交数据库会话
+        flash('Item updated.')
+        return redirect(url_for('hello')) # 重定向回主页
+
+    return render_template('edit.html', movie=movie) # 传入被编辑 的电影记录
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST']) # 限定只接受 POST 请求
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id) # 获取电影记录
+    db.session.delete(movie) # 删除对应的记录
+    db.session.commit() # 提交数据库会话
+    flash('Item deleted.')
+    return redirect(url_for('hello')) # 重定向回主页
+
 
 @app.errorhandler(404) # 传入要处理的错误代码
 def page_not_found(e): # 接受异常对象作为参数
@@ -44,4 +93,4 @@ def page_not_found(e): # 接受异常对象作为参数
 
 
 if __name__ == '__main__':
-    app.run(Debug=True)
+    app.run()
--- config.py
+++ config.py
@@ -5,6 +5,5 @@ PASSWORD = '20000520'
 HOST = '127.0.0.1'
 PORT = '3306'
 DATABASE = 'flask_demo'
-
-SQLALCHEMY_DATABASE_URI = "{}+{}://{}:{}@{}:{}/{}?charset=utf8".format(DIALECT, DRIVER, USERNAME, PASSWORD, HOST, PORT,
-                                                                       DATABASE)
+SECRET_KEY = 'TPmi4aLWRbyVq8zu9v82dWYW1'
+SQLALCHEMY_DATABASE_URI = "{}+{}://{}:{}@{}:{}/{}?charset=utf8".format(DIALECT, DRIVER, USERNAME, PASSWORD, HOST, PORT,DATABASE)
